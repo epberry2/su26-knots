0910c4b8f5fb652eb5ee589dc66828cfbf8fbb2d
hzaslow <hzaslow2@illinois.edu>
2026-06-01 12:19:09 -0500

    First method!

---
+++ continuedfrac.py
@@ -0,0 +1,25 @@
+import numpy
+
+def continuedfrac(a, b): 
+    X = []
+    num = a
+    denom = b
+    if num >= b:
+        c = num // denom
+        X.append(c)
+        num -= (c * denom)
+    parity = 0
+    while (num != 0):
+        num, denom = denom, num
+        if parity == 0:
+            num -= 1
+        c = num // denom
+        X.append(c)
+        if parity == 0:
+            num += 1
+        num -= (c * denom)
+        parity = 1 - parity
+    return X
+
+
+print(continuedfrac(5,12))
