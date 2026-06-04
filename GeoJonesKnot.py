36a7ffd130a1d719f0e04ba29ff50e3d36ab2014
hzaslow <hzaslow2@illinois.edu>
2026-06-04 13:30:39 -0500

    rearranged stuff

---
+++ GeoJonesKnot.py
@@ -0,0 +1,47 @@
+from findloops import findloops
+from findloops import findpath
+from findloops import findpathwithloops
+from sympy import symbols, Poly, gcd
+from JonesPolyAlg import AlgBracket, AlgJonesKnot
+from GeoJonesKnotHelpers import *
+from tanglestate import get_state
+
+q = symbols('q')
+
+
+def geoJonesKnot(num, denom):
+    # Strategy: Walk along loop with right hand to wall, keeping track of how many times you have looped
+    # around each point. At the end, handle the figure-8 loop, and then return.
+    # If a portion of the figure-8 loop ends with a half C-arc, then the curve
+    # obtained by following the figure-8 half-loop with RH to wall for that portion
+    # will wind an extra time around the middle point due to the half-loop iff the half-loop is CCW
+    # when RH is to wall; likewise, for a half R-arc, iff the half-loop is CW.
+    # Therefore, we include the arcs in the monomials at the midpoints depending on their orientation.
+    
+    assert num >= denom, "only rationals >= 1 considered at the moment"
+    assert num % 2 == 1, "only odd numerators considered at the moment"
+    
+    orientation, points_ = get_state(num, denom)
+    assert orientation == "UP" or orientation == "OP", "tangle has RI orientation"
+    
+    path, loops = findpathwithloops(num, denom)
+    curr_state = CurrState(points = points_) # stores winding number around each vertex, as well as lowest power to normalize
+    monomial_array = [(0, 0)] * num
+    tracePath(num, denom, path, loops, curr_state, monomial_array)
+    # handle endpoint
+    if denom % 2 == 0: 
+        curr_state.increment_r(-1) # loop around endpoint first
+        idx = 0
+        parity = -1
+    else:
+        idx = num - 1
+        parity = 1
+    store_monomial(monomial_array, curr_state, idx, parity) # parity = 1: RH is to wall
+    if denom % 2 == 1: curr_state.increment_r(-1) 
+    # walk backwards
+    tracePath(num, denom, list(reversed(path)), list(reversed(loops)), curr_state, monomial_array)
+    # done!
+    jones_polynomial = Poly(0, q)
+    for coeff, exp in monomial_array:
+        jones_polynomial += Poly(coeff * q ** (exp - curr_state.lowest_power), q)
+    return jones_polynomial
--- JonesPolyGeoHelpers.py
+++ JonesPolyGeoHelpers.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field
+
 
 def intersectionAboveAxis(num, denom, point):
     if point <= num // 2:
@@ -25,65 +25,4 @@ def coeff(pathType):
         return 1
     return -1
 
-@dataclass
-class CurrState:
-    points: tuple[str, str, str]
-    lowest_power: int = 0
-    winding_nums: list[int] = field(default_factory=lambda: [0, 0, 0])
-    arc_right: bool = field(init = False)
-
-    def __post_init__(self):
-        self.arc_right = (self.points[1] == "X+")
-        
-    def increment_l(self, amount = 1):
-        self.winding_nums[0] += amount
-        
-    def increment_c(self, amount = 1):
-        self.winding_nums[1] += amount
-        
-    def increment_r(self, amount = 1):
-        self.winding_nums[2] += amount
-    
-    def power(self):
-        pow = -1 * 2 * (self.winding_nums[0] + self.winding_nums[1] + self.winding_nums[2])
-        self.lowest_power = min(self.lowest_power, pow)
-        return pow
-        
-
-def store_monomial(monomials, state, index, parity):
-    if state.arc_right: parity *= -1
-    monomials[index] = (parity, state.power())
-    
-def intersection_index(num, denom, point, next_point, path_type, parity):
-    if path_type == "C":
-        index = 2 * (min(point, next_point) - 1) + int(denom % 2 == 0)
-    elif path_type == "R":
-        index = 2 * (min(point, next_point) - 1 - num) + (num - denom)
-    else:
-        raise ValueError(f"Only intersect at C or R arcs")
-    return index + int(parity == 1) # We walk with RH to wall, so on right iff parity = 1
-
-def tracePath(num, denom, path, loops, state, monomial_array):
-    for point, next_point, path_type in zip(path, path[1:], loops):   # fancy for loop
-        if path_type == "T": continue
-        is_ccw = rotateCCW(num, denom, point, next_point, path_type)
-        parity = -1 if (point > next_point) ^ (path_type == "C") else 1
-        if path_type == "L":
-            state.increment_l(-1 if is_ccw else 1)
-        if path_type == "C":
-            if is_ccw:
-                state.increment_c(-1) # increment beforehand
-                intersection_num = intersection_index(num, denom, point, next_point, path_type, parity)
-                store_monomial(monomial_array, state, intersection_num, parity)
-            else:
-                intersection_num = intersection_index(num, denom, point, next_point, path_type, parity)
-                store_monomial(monomial_array, state, intersection_num, parity)
-                state.increment_c(1) # increment aftwerwards
-        if path_type == "R":
-            intersection_num = intersection_index(num, denom, point, next_point, path_type, parity)
-            if not is_ccw: # increment beforehand
-                state.increment_r(1)
-            store_monomial(monomial_array, state, intersection_num, parity)
-            if is_ccw: # increment afterwards
-                state.increment_r(-1)
 
