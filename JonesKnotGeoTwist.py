3f81ff2d08a45340dd4c95dac024d4f647fc496c
epberry2 <epberry2@illinois.edu>
2026-06-05 12:59:36 -0500

    Added test file for jones knot polynomial

--- JonesKnotGeoTwist.py
+++ JonesKnotGeoTwist.py
@@ -8,13 +8,15 @@ def GeoJonesKnot(num,denom):
     u = num - denom # take inverse top twist, so u/v = (u-v)/v
     v = denom
     path, loops = fl.findpathwithloops(u, v)
-
-    
+    #print(path)
     jones = 1
     currPow = 0
     
     sign = 0 # we change sign every L or R loop
     qsign = 1 # the sign of the coefficients
+
+
+    
     for i in range(len(loops)):
         
         if loops[i] == 'L':
@@ -33,40 +35,50 @@ def GeoJonesKnot(num,denom):
         
         elif loops[i] == 'C':
             if path[i+1] < path[i]:
-                currPow += 2
-            else:
                 currPow -= 2
-
+                if path[i] > u // 2 and path[i+1] < u // 2:
+                    sign = 1 - sign
+            else:
+                currPow += 2
+                if path[i+1] > u // 2 and path[i] < u // 2:
+                    sign = 1 - sign
         else:
+            # T arc
             if path[i+1] > path[i]:
-                currPow -= 2
-                if (path[i+1] - u) <= -(-v // 2) and sign == 1 or (path[i+1] - u) > -(-v // 2) and sign == 0:
-                    sign = sign - 1
+                currPow -= 2            
+                if (path[i+1] - u) < (v + 1) / 2 and sign == 1 or (path[i+1] - u) > (v + 1) / 2 and sign == 0:
+                    sign = 1 - sign
             else:
                 currPow += 2
-                if path[i+1] <= -(-u // 2) and sign == 1 or path[i+1] > -(-u // 2) and sign == 0:
-                    sign = sign - 1
+                if path[i+1] <= (u + 1) / 2 and sign == 1 or path[i+1] > (u + 1) / 2 and sign == 0:
+                    sign = 1 - sign
 
 
         if sign == 0:
             jones += (-1)**qsign * q**currPow
+
         
         qsign = 1 - qsign
+        #print(currPow, sign)
         
             
     # now we have traversed the outside blue path, so traverse the inside segment in reverse
-    if u % 2 == 0:
+    
+    if u < v:
         currPow += 2
     else:
         currPow -= 2
+        sign = 1 - sign
     
-    sign = 1 - sign
     if sign == 0:
         jones += (-1)**qsign * q**currPow
+
     qsign = 1 - qsign
     loops.reverse()
     path.reverse()
 
+    #print(currPow, sign)
+
     for i in range(len(loops)):
         
         if loops[i] == 'L':
@@ -85,24 +97,31 @@ def GeoJonesKnot(num,denom):
 
         elif loops[i] == 'C':
             if path[i+1] < path[i]:
-                currPow += 2
-            else:
                 currPow -= 2
+                if path[i] > u // 2 and path[i+1] < u // 2:
+                    sign = 1 - sign
+            else:
+                currPow += 2
+                if path[i+1] > u // 2 and path[i] < u // 2:
+                    sign = 1 - sign
 
         else:
+            # T arc
             if path[i+1] > path[i]:
-                currPow -= 2
-                if (path[i+1] - u) <= -(-v // 2) and sign == 1 or (path[i+1] - u) > -(-v // 2) and sign == 0:
-                    sign = sign - 1
+                currPow -= 2            
+                if (path[i+1] - u) < (v + 1) / 2 and sign == 1 or (path[i+1] - u) > (v + 1) / 2 and sign == 0:
+                    sign = 1 - sign
             else:
                 currPow += 2
-                if path[i+1] <= -(-u // 2) and sign == 1 or path[i+1] > -(-u // 2) and sign == 0:
-                    sign = sign - 1
+                if path[i+1] <= (u + 1) / 2 and sign == 1 or path[i+1] > (u + 1) / 2 and sign == 0:
+                    sign = 1 - sign
             
         if sign == 0:
+
             jones += (-1)**qsign * q**currPow
 
         qsign = 1 - qsign
+        #print(currPow, sign)
 
     powers = []
     for term in jones.as_coefficients_dict().keys():
@@ -116,6 +135,6 @@ def GeoJonesKnot(num,denom):
     min_power = min(powers)
     jones = (q**(-min_power) * jones).expand()
 
-    return sp.Poly(jones)
+    return sp.Poly(jones, q)
 
-print(GeoJonesKnot(9,4))
+print(GeoJonesKnot(5,3))
--- JonesTest.py
+++ JonesTest.py
@@ -4,6 +4,8 @@ from itertools import permutations
 
 import JonesPolyAlg as jpa
 import JonesPolyGeo as jpg
+import JonesKnotGeoTwist as jpgt
+
 
 def coprime_pairs(n):
     numbers = range(1, n + 1)
@@ -12,19 +14,35 @@ def coprime_pairs(n):
     
     return coprime_pairs
 
-num_correct = 0
-pairs = coprime_pairs(20)
-for i, j in pairs:
-    va = jpa.AlgBracket(i, j)
-    vb = jpg.JonesPolyGeo(i, j)
+def test_bracket(n):
+    num_correct = 0
+    pairs = coprime_pairs(n)
+    for i, j in pairs:
+        va = jpa.AlgBracket(i, j)
+        vb = jpg.JonesPolyGeo(i, j)
 
-    correct = va[0] == vb[0] or -va[0] == vb[0] and va[1] == vb[1] or -va[1] == vb[1]
-    print(f"({i}, {j}): {correct}")
-    if correct:
-        num_correct += 1
-    else:
-        print(va)
-        print(vb)
+        correct = va[0] == vb[0] or -va[0] == vb[0] and va[1] == vb[1] or -va[1] == vb[1]
+        print(f"({i}, {j}): {correct}")
+        if correct:
+            num_correct += 1
+        else:
+            print(va)
+            print(vb)
 
-print(f"Num correct: {num_correct} / {len(pairs)}, {(num_correct / len(pairs)):.2f} ")
+    print(f"Num correct: {num_correct} / {len(pairs)}, {(num_correct / len(pairs)):.2f} ")
+
+def test_knot(n):
+    num_correct = 0
+    for i in range(1,n):
+        va = jpa.AlgJonesKnot(n, i)
+        vb = jpgt.GeoJonesKnot(n, i)
+        correct = va == vb or va == -vb
+        print(f"({n}, {i}): {correct}")
+        if correct:
+            num_correct += 1
+        else:
+            print(va)
+            print(vb)
+
+test_knot(7)
     
