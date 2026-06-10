a4864627248fbb90d0201a68763f140263f37ccd
guorongjiang <guorongjiang34@gmail.com>
2026-06-10 15:08:47 -0500

    m

--- colored_homfly/homfalgjvaried.py
+++ colored_homfly/homfalgjvaried.py
@@ -925,9 +925,9 @@ def smoke_test() -> None:
 
 if __name__ == "__main__":
     # Change these three numbers to test.
-    num = 5
+    num = 3
     denom = 1
-    j = 2
+    j = 1
 
     # Tangle evaluation of tau_{num/denom}.
     ev = evaluate_tangle_matrix(num, denom, j, verbose=False)
