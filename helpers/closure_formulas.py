11a66f081be05ac06e4aa1cef3225c75ea795e4d
hzaslow <hzaslow2@illinois.edu>
2026-06-10 14:23:35 -0500

    Added closure twist formulas

--- helpers/closure_formulas.py
+++ helpers/closure_formulas.py
@@ -24,6 +24,26 @@ def cl_num(orientation, j, k):
             
 
 def cl_denom(orientation, j, k):
-    "Warning: Denominator closure"
-    return
+    "Denominator closure not implemented"
+    raise NotImplementedError
+
+def cl_t_twist(orientation, j, k):
+    """
+    If orientation == "UP", returns Cl(TUP[j, k])
+    If orientation == "RI", returns Cl(TRI[j, k])
+    """
+    match orientation:
+        case "UP":
+            out_num = (-q)**k * q**(2 * k**2)
+            out_denom = qnums.qpochhammer(q**2, q**2, k)
+            out_num *= qnums.qpochhammer(a**2 * q**(2 - 2 * j - 2 * k), q**2, j)
+            return sp.factor(sp.simplify(out_num * qnums.qbinom(j, k) / out_denom))
+        case "RI":
+            out_num = q**(2 * (j - k)**2)
+            out_denom = (-q)**(j - k) * a**(2 * (j - k))
+            out_num *= qnums.qpochhammer(a**2 * q**(2 - 2 * j - 2 * (j - k)), q**2, j - k)
+            out_denom *= qnums.qpochhammer(q**2, q**2, j - k)
+            return sp.factor(sp.simplify(out_num * qnums.qbinom(j, k) / out_denom))
+        case _:
+            raise TypeError("Orientation not UP or RI")
 
