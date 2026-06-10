e9439bab72ce2b1377422ada9e7d7af6d084a8bb
epberry2 <epberry2@illinois.edu>
2026-06-10 15:49:11 -0500

    updated main

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from helpers.visualize import visualize_lattice
 from colored_homfly.knot_closure import colored_homfly_knot
 
 poly = sp.Poly(colored_homfly_knot(11, 8, 3))
-visualize_lattice(poly, path="9-7-3", show_plot=False)
+visualize_lattice(poly, path="knot", show_plot=False)
