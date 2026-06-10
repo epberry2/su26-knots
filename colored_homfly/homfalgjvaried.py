24b14b824ceabc928146cccb93a38128259a9489
guorongjiang <guorongjiang34@gmail.com>
2026-06-10 14:48:12 -0500

    m

--- colored_homfly/homfalgjvaried.py
+++ colored_homfly/homfalgjvaried.py
@@ -927,7 +927,7 @@ if __name__ == "__main__":
     # Change these three numbers to test.
     num = 5
     denom = 1
-    j = 10
+    j = 2
 
     # Tangle evaluation of tau_{num/denom}.
     ev = evaluate_tangle_matrix(num, denom, j, verbose=False)
