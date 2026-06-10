392fff6e01fd935b781c81facbde91b072a76702
epberry2 <epberry2@illinois.edu>
2026-06-10 15:48:36 -0500

    updated visualizer to save file

--- helpers/visualize.py
+++ helpers/visualize.py
@@ -6,18 +6,21 @@ import sympy as sp
 q = sp.symbols('q')
 a = sp.symbols('a')
 
-def visualize_lattice(polynomial):
+def visualize_lattice(polynomial, path=None, show_plot=True):
     p_dict = polynomial.as_dict()
     keys = p_dict.keys()
     x, y = zip(*keys)
     z = p_dict.values()
-    scatter = plt.scatter(x, y, c=z, cmap='viridis', s=100, edgecolor='black')
+    scatter = plt.scatter(x, y, c=z, cmap='viridis', s=50, edgecolor='black')
     plt.xlabel('q powers')
     plt.ylabel('a powers')
     colorbar = plt.colorbar(scatter)
     colorbar.set_label('coefficient of monomial')
     plt.title('Lattice points of Homfly Polynomial')
-    plt.show()
+    if path is not None:
+        plt.savefig(f"{path}.png")
+    if show_plot:
+        plt.show()
     
 
 #polynomial = sp.Poly(colored_homfly_knot(3, 1, 2), q, a)
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import sympy as sp
 from helpers.visualize import visualize_lattice
 from colored_homfly.knot_closure import colored_homfly_knot
 
-poly = sp.Poly(colored_homfly_knot(3, 1, 2))
-visualize_lattice(poly)
+poly = sp.Poly(colored_homfly_knot(11, 8, 3))
+visualize_lattice(poly, path="9-7-3", show_plot=False)
