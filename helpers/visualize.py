ef71c06ed44bb4fd3d6960cd2a5b6d06968828ea
epberry2 <epberry2@illinois.edu>
2026-06-16 09:53:22 -0500

    fixed visualization axis

--- helpers/visualize.py
+++ helpers/visualize.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 import numpy as np
 import sympy as sp
 
@@ -6,16 +7,23 @@ import sympy as sp
 q = sp.symbols('q')
 a = sp.symbols('a')
 
-def visualize_lattice(polynomial, path=None, show_plot=True):
+def visualize_lattice(polynomial, path=None, show_plot=True, log_scale=True, edgecolor=None):
     p_dict = polynomial.as_dict()
     keys = p_dict.keys()
     x, y = zip(*keys)
     z = p_dict.values()
-    scatter = plt.scatter(x, y, c=z, cmap='viridis', s=50, edgecolor='black')
+    norm = colors.SymLogNorm(linthresh=1.0, linscale=1.0, vmin=min(z), vmax=max(z))
+    if log_scale:
+        scatter = plt.scatter(y, x, c=z, cmap='viridis', norm=norm, s=50, edgecolor=edgecolor)
+        colorbar = plt.colorbar(scatter)
+        colorbar.set_label('coefficient of monomial (log scale)')
+    else:
+        scatter = plt.scatter(y, x, c=z, cmap='viridis', s=50, edgecolor=edgecolor)
+        colorbar = plt.colorbar(scatter)
+        colorbar.set_label('coefficient of monomial')
     plt.xlabel('q powers')
     plt.ylabel('a powers')
-    colorbar = plt.colorbar(scatter)
-    colorbar.set_label('coefficient of monomial')
+    
     plt.title('Lattice points of Homfly Polynomial')
     if path is not None:
         plt.savefig(f"{path}.png")
