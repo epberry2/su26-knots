c3573e1b2dff3d512148bb956b63ac4a6290cc94
hzaslow <hzaslow2@illinois.edu>
2026-06-17 10:50:39 -0500

    fixed bugs and added capability to print min vecs

--- quivers/quiv_test.py
+++ quivers/quiv_test.py
@@ -2,31 +2,32 @@ from quivers.knot_quiver import colored_jones_vector_and_quiver, colored_homfly_
 from helpers.quantum_algebra import QuantumCombinatorics
 import numpy as np
 import sympy as sp
+from bisect import insort
 from quivers.evaluate_quiver import get_tuples
 
-def print_min_vecs(u, v, n):
-    S, A, Q = colored_homfly_vectors_and_quiver(u, v)
+def print_min_vecs(u, v, n, m=1):
+    S, A, Q = colored_homfly_vectors_and_quiver(u, v) # returns Tuple[List[int], List[int], Matrix]
     print(S)
     sp.pprint(Q)
     S = np.array(S)
     A = np.array(A)
     for j in range(1, n + 1):
-        combos = get_tuples(u, j)
+        combos = get_tuples(u, j) # returns list[tuple] of vectors of length u which partition j
         combo_list = list(combos)
-        min_pow = 0
-        min_d = (0) * u
+        min_d = [(np.zeros(u, dtype=int), 0) for _ in range(m)]
         flag = False
         for d in combo_list:
             d = np.array(d)
             p1 = np.dot(S, d)        
             # p2 = np.dot(A, d)
             p3 = np.einsum('i,ij,j', d, Q, d)
-            if not flag or p1 + p3 < min_pow:
-                min_pow = p1 + p3
-                min_d = d
+            if not flag or p1 + p3 < min_d[m - 1][1]:
+                insort(min_d, (d, p1 + p3), key=lambda x: x[1])
+                min_d.pop()
             flag = True
-        print(min_d)      
-def print_max_vecs(u, v, n):
+        print([(vec.tolist(), val) for vec, val in min_d])
+        print("-------------------------")
+def print_max_vecs(u, v, n, m=1):
     S, A, Q = colored_homfly_vectors_and_quiver(u, v)
     print(S)
     sp.pprint(Q)
@@ -52,7 +53,12 @@ def print_max_vecs(u, v, n):
                 flag = True
         print(max_d)
 
-u, v = 3, 1
+u, v = 13, 8
 printnum = 10
 
-print_min_vecs(u, v, printnum)
+S, A, Q = colored_homfly_vectors_and_quiver(u, v)
+sp.pprint(Q)
+
+
+
+# print_min_vecs(u, v, 30, 10)
