d9a4a81ff9961a7735f182aabfaffa8055b27ce4
epberry2 <epberry2@illinois.edu>
2026-06-18 13:40:20 -0500

    added func to evalute quiver with qsub

--- quivers/evaluate_quiver.py
+++ quivers/evaluate_quiver.py
@@ -151,6 +151,36 @@ def evaluate_quiver_jones(Q, H, u, v, j):
 
     return sp.Poly(dict(normalized), q)
 
+def evaluate_quiver_qsub(Q, S, A, u, v, j, qsub):
+    # Evaluates the j colored jones polynomial given the K_(u/v) quiver (homfly quiver)
+    qc = QuantumCombinatorics() # creates cache for quantum multinomials
+    poly = defaultdict(int)
+    S = np.array(S)
+    A = np.array(A)
+    combos = get_tuples(u, j) # get all u-tuples with values adding up to j
+    combo_list = list(combos)
+
+    for d in tqdm(combo_list, desc="Processing tuples", unit="tuple"):        
+        d = np.array(d)
+        p1 = np.dot(-S, d)   
+        p2 = np.dot(qsub * A, d)
+        p3 = np.einsum('i,ij,j', d, -Q, d)
+        multinom = qc.get_multinomial(d)
+        sign = (-1) ** (p1 % 2)
+        for i, coeff in enumerate(multinom):
+            if coeff != 0:
+                poly[int(p1+p2+p3-(2*i))] += int(sign * coeff)
+
+    # normalize polynomial so lowest powers are 0
+
+    min_value_key = min(poly)
+    normalized = {
+        k - min_value_key: val 
+        for k, val in poly.items()
+    }
+
+    return sp.Poly(dict(normalized), q)
+
 def evaluate_quiver_jones_from_homfly(Q, S, A, u, v, j):
     # Evaluates the j colored jones polynomial given the K_(u/v) quiver (homfly quiver)
     qc = QuantumCombinatorics() # creates cache for quantum multinomials
